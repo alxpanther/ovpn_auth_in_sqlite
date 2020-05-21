2c0337f1ce324a8415b489ebb24b27316da33c1e
nuxster <nuxster@jupiter.domain.local>
2020-05-21 16:27:43 +0300

    2020.05.21

--- auth.py
+++ auth.py
@@ -4,7 +4,7 @@
 import sqlite3
 import argparse
 import getpass
-import hashlib
+import hashlib, secrets
 import os
 import re
 
@@ -55,12 +55,9 @@ class DBWorker():
             return(password)
         # Прорверка пароля при авторизации в OpenVPN.
         else:
-            password = hashlib.sha512(bytes(password, 'utf-8')).digest()
-            #password2 = hashlib.sha512(bytes(password2, 'utf-8')).digest()
-            if password != password2:
-                return 0
-            else:
-                return 1
+            # password2 полученные из БД имеет тип str, чтобы корректно сравнить пароли password в str.
+            password = str(hashlib.sha512(bytes(password, 'utf-8')).digest())
+            return password == password2
 
     def db_init(self, args=False):
         '''
@@ -88,7 +85,8 @@ class DBWorker():
         if (pattern.fullmatch(login) is None) or (len(login) < 3):
             print('Не корректное имя пользователя!')
             exit(1)
-        if (self.curs.execute(f'SELECT "login" FROM "users" WHERE "users"."login" = "{login}";').fetchall()):
+
+        if (self.curs.execute(f'SELECT "login" FROM "users" WHERE "users"."login" = "{login}";').fetchone()):
             return(True)
         return(False)
 
@@ -134,6 +132,15 @@ class DBWorker():
             except sqlite3.OperationalError:
                 print('Ошибка записи в БД.')
                 exit(1)
+    
+    def db_user_isactive(self, login):
+        '''
+        Активна ли учетная запись пользователя.
+        '''
+        if self.curs.execute(f'SELECT "active" FROM "users" WHERE "users"."login" = "{login}";').fetchone()[0]:
+            return 1
+        else:
+            return 0
 
     def db_change_pwd(self, args):
         '''
@@ -156,7 +163,7 @@ class DBWorker():
         Информация о пользователе из базы.
         '''
         if self.db_chk_user(args.login):
-            if self.curs.execute(f'SELECT "active" FROM "users" WHERE "users"."login" = "{args.login}";').fetchall():
+            if self.db_user_isactive(args.login):
                 print(f'Пользователь {args.login} активен.')
                 exit(0)
             else:
@@ -170,16 +177,14 @@ class DBWorker():
         Авторизация в OpenVPN.
         '''
         authfile = open(args.file, 'r')
-        # login, passord = authfile.readline().split('@')
         login, password = [i.strip() for i in authfile.readlines()]
         authfile.close()
 
-        if self.db_chk_user(login):
-            password2 = self.curs.execute(f'SELECT "password" FROM "users" WHERE "users"."login" = "{login}";').fetchall()
+        if self.db_chk_user(login) and self.db_user_isactive(login):
+            password2 = self.curs.execute(f'SELECT "password" FROM "users" WHERE "users"."login" = "{login}";').fetchone()[0]
             if self.pass_check(not_auth=False, password=password, password2=password2):
                 return 0
         return 1
-
 
 if __name__ == '__main__':
     db = DBWorker()
@@ -220,9 +225,9 @@ if __name__ == '__main__':
     ovpnauth_parser.add_argument('file', metavar='auth-file', help='Файл для авторизации')
     ovpnauth_parser.set_defaults(func=db.auth)
 
-
     args = parser.parse_args()
 
+    # Выводить подсказку если скрипт запущен без параметров.
     if not vars(args):
         parser.print_usage()
     else:
